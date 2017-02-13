1c6adf255079a7c73c7034c6e94c727218420ce8
Metallou <kevincastejon13@gmail.com>
2017-02-13 17:25:26 +0100

    fixed blank files

---
+++ js/gmaps.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const ready = function()
+{
+
+    let WideDiv = document.getElementsByClassName("wide");
+    let TopDiv = document.getElementsByClassName("top");
+    let RightDiv = document.getElementsByClassName("right");
+    let BottomDiv = document.getElementsByClassName("bottom");
+    let LeftDiv = document.getElementsByClassName("left");
+    let div = document.getElementsByTagName("div");
+
+    let TopArray = 0;
+    let RightArray = 0;
+    let BottomArray = 0;
+    let LeftArray = 0;
+}
+document.addEventListener("DOMContentLoaded", ready)
