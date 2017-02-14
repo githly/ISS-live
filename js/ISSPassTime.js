33f62b4fdde5fe10e846c8fa3b297ab0efad0ac4
Metallou <kevincastejon13@gmail.com>
2017-02-14 16:15:45 +0100

    M2: #36 fonctions rewritten, all working

--- js/ISSPassTime.js
+++ js/ISSPassTime.js
@@ -9,22 +9,47 @@ const callISSPassTimeScript = function(lat, lon)
 }
 const ISSPassTimeScriptCallback = function(data)
 {
-    console.log(data);
-    document.getElementById("TEST").children[1].textContent = data.response[0].duration;
-    CallUNIXConvertTimestampScript(data.response[0].risetime);
+    displayTime(data.response[0].risetime);
+    displayDuration(data.response[0].risetime);
+    document.getElementById("duration").textContent = data.response[0].duration + " seconds";
 }
 
-const CallUNIXConvertTimestampScript = function(timestamp)
+const convertDate = function(d)
 {
-    const script = document.createElement("script");
-    script.setAttribute("src", "http://www.convert-unix-time.com/api?timestamp="+timestamp+"&format=german&timezone=Paris&returnType=jsonp&callback=UNIXConvertTimestampScriptCallback");
-    document.body.appendChild(script);
-    script.parentNode.removeChild(script);
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+    let year = d.getFullYear();
+    let month = months[d.getMonth()];
+    let date = d.getDate();
+    let hour = d.getHours();
+    let min = d.getMinutes();
+    let sec = d.getSeconds();
+    let time = date + '-' + month + '-' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
 }
-const UNIXConvertTimestampScriptCallback = function(data)
+const convertDuration = function(d)
 {
-    console.log(data);
-    document.getElementById("TEST").children[3].textContent = data.localDate;
+    let time = d;
+    let hour = Math.floor(time/3600);
+    time = time%3600;
+    let min = Math.floor(time/60);
+    time = time%60;
+    let sec = time;
+    time = hour +":"+ min +":"+ sec;
+    return time;
+}
+
+const displayTime = function(timestamp)
+{
+    let datetimestamp = new Date(timestamp*1000);
+    document.getElementById("time").textContent = convertDate(datetimestamp);
+}
+const displayDuration = function(timestamp)
+{
+    let datenow = new Date();
+    let datetimestamp = new Date(timestamp*1000);
+    let duration = Math.floor((datetimestamp.getTime() - datenow.getTime())/1000);
+    document.getElementById("countdown").textContent = convertDuration(duration);
 }
 
 const ISSPassTimeReady = function()
