5ea5b8800920a90c870c02f4acc4db012f45e673
LaBete <babaorhum001@gmail.com>
2017-02-14 17:56:41 +0100

    add markers

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,11 +1,35 @@
-
-
 function initMap() {
-  var mapOptions = {
-    zoom: 18,
-    center: {lat: 43.342052, lng: 5.438830},
-    disableDefaultUI: true
-  }
-  var map = new google.maps.Map(document.getElementById("map"),
-       mapOptions);
+  var SIMPLonMARS = {
+      lat: 43.342052,
+      lng: 5.438830
+  };
+
+    var mapOptions = {
+        zoom: 18,
+        center: SIMPLonMARS,
+        disableDefaultUI: true
+    }
+    var map = new google.maps.Map(document.getElementById("map"),
+        mapOptions);
+
+
+
+  // This event listener calls addMarker() when the map is clicked.
+  google.maps.event.addListener(map, 'click', function(event) {
+    addMarker(event.latLng, map);
+  });
+
+  // Add a marker at the center of the map.
+  addMarker(SIMPLonMARS, map);
+
+
+// Adds a marker to the map.
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
 }
