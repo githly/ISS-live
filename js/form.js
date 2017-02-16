1879be2c65948ae63c4c494e687040230a566c7d
githly <bergmanlaurene@gmail.com>
2017-02-16 11:43:32 +0100

    add clear form

--- js/form.js
+++ js/form.js
@@ -6,14 +6,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let longitude = 0;
 // ecoute la validation du formulaire
   document.getElementById('form').addEventListener("submit", function (evt){
-    evt.preventDefault();
+
     lattitude = document.getElementById('lattitude').value;// Recupere la lattitude
     longitude = document.getElementById('longitude').value; // Recupere la longitude
     console.log(lattitude);
     console.log(longitude);
+
+    /*if {
+
+    }*/
   });
 
-
+  // clear le formulaire
+  document.getElementById('clear').addEventListener("click", function (evt){
+    document.getElementById('form').reset();
+  });
 
 
 
