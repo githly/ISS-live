9ed7004259041fa0596dbe891d0621d102b52ca4
LaBete <babaorhum001@gmail.com>
2017-02-16 10:40:42 +0100

    B3 (#8) - keep 1 marker on the map

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,35 +1,57 @@
+// In the following example, markers appear when the user clicks on the map.
+// The markers are stored in an array.
+// The user can then click an option to hide, show or delete the markers.
+var map;
+var markers = [];
+
 function initMap() {
-  var SIMPLonMARS = {
-      lat: 43.342052,
-      lng: 5.438830
-  };
-
-    var mapOptions = {
-        zoom: 18,
-        center: SIMPLonMARS,
-        disableDefaultUI: true
-    }
-    var map = new google.maps.Map(document.getElementById("map"),
-        mapOptions);
-
-
-
-  // This event listener calls addMarker() when the map is clicked.
-  google.maps.event.addListener(map, 'click', function(event) {
-    addMarker(event.latLng, map);
+  var SIMPLonMARS = {lat: 43.342052, lng: 5.438830 };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: SIMPLonMARS,
+    zoom: 18,
+    disableDefaultUI: true
   });
 
-  // Add a marker at the center of the map.
-  addMarker(SIMPLonMARS, map);
+  // This event listener will call addMarker() when the map is clicked.
+  map.addListener('click', function(event) {
+    addMarker(event.latLng);
+  });
 
+  // Adds a marker at the center of the map.
+  addMarker(SIMPLonMARS);
 
-// Adds a marker to the map.
-function addMarker(location, map) {
-  // Add the marker at the clicked location, and add the next-available label
-  // from the array of alphabetical characters.
+}
+
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
   var marker = new google.maps.Marker({
     position: location,
     map: map
   });
+  deleteMarkers();
+  markers.push(marker);
+
 }
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
 }
