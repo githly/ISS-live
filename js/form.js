bf98fa816a1cfa8449a6136adc511b1fa4b245f3
githly <bergmanlaurene@gmail.com>
2017-02-17 09:10:54 +0100

    add select in input form"

--- js/form.js
+++ js/form.js
@@ -2,20 +2,73 @@
 // Demarre le Js quand le DOM est chargé
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  let lattitude =0;
+  let latitude =0;
   let longitude = 0;
 // ecoute la validation du formulaire
   document.getElementById('form').addEventListener("submit", function (evt){
 
-    lattitude = document.getElementById('lattitude').value;// Recupere la lattitude
+    latitude = document.getElementById('latitude').value;// Recupere la latitude
     longitude = document.getElementById('longitude').value; // Recupere la longitude
-    console.log(lattitude);
+    console.log(latitude);
     console.log(longitude);
 
-    /*if {
+    /*document.getElemntsbyClassName('selected')
+    document.getElementById("form").elements["latitude"].value = '1234567';
+
+    if {
 
     }*/
-  });
+    // Select
+
+    // Injecter les valeur dans les input
+    /*if {
+      document.getElemntsbyClassName('selected').options.index
+    }
+
+    document.getElementById("form").options[document.getElemntsbyClassName('selected').selectedIndex].text;*/
+
+
+  });// End Submit
+  // tableau double dimension
+  let getPlaces = function () {
+    // INitialise les paramètres de l'objet
+        let places = function (name, latitude, longitude){
+          this.name=name;
+          this.latitude=latitude;
+          this.longitude=longitude;
+        }
+        // definir les objets
+        let MARSEILLE = new places("MARSEILLE", 12345, 5678);
+        let NY = new places("NY", 8745, 35689);
+        let PERTH = new places("PERTH", 8745, 35689);
+        let PAP = new places("PAP", 8745, 35689);
+        let BA = new places("BA", 8745, 35689);
+        let CC = new places("CC", 8745, 35689);
+        // Ajouter les objets au tableau
+        let selectList = [MARSEILLE, NY, PERTH, PAP, BA, CC];
+        return selectList;
+      }
+      // ecoute l'élement select et si il change ...
+document.getElementById('select').addEventListener("change", function (evt){
+
+
+let select = document.getElementById('select');
+let index = select.selectedIndex;
+let inputLatitude = document.getElementById('latitude');// Recupere la latitude
+let inputLongitude = document.getElementById('longitude'); // Recupere la longitude
+console.log(select.options[index].value);
+console.log(index);
+console.log();
+let place = getPlaces();
+  if (select.options[index].value){ // si tt les autres cas
+    console.log(place[index-1]);
+    inputLatitude.value = place[index-1].latitude;
+    inputLongitude.value = place[index-1].longitude;
+  }else { // SI on est sur index 0 ou chaine vide
+
+  }
+
+}); // end CHange
 
   // clear le formulaire
   document.getElementById('clear').addEventListener("click", function (evt){
