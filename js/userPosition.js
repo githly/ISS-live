04271b609d2bc64fa870acd000a81c83938e64ec
Metallou <kevincastejon13@gmail.com>
2017-02-17 10:38:19 +0100

    renamed a js file, fixed floating digits, rewrote html display structure

--- js/userPosition.js
+++ js/userPosition.js
@@ -5,7 +5,7 @@ let uLon = 0.0;
 
 const userPositionReady = function()
 {
-    document.getElementById("userLatitude").textContent = uLat.toFixed(10);
-    document.getElementById("userLongitude").textContent = uLon.toFixed(10);
+    document.getElementById("userLatitude").textContent = uLat.toFixed(4);
+    document.getElementById("userLongitude").textContent = uLon.toFixed(4);
 }
 document.addEventListener("DOMContentLoaded", userPositionReady);
