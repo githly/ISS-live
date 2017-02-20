831deb8135e967554d40fd48e272ab8bb5749039
Metallou <kevincastejon13@gmail.com>
2017-02-20 09:26:08 +0100

    changed script loading order

--- js/gmaps.js
+++ js/gmaps.js
@@ -2,8 +2,8 @@
 
 function initMap() {
   var mapOptions = {
-    zoom: 18,
-    center: {lat: 43.342052, lng: 5.438830},
+    zoom: 2,
+    center: {lat: SimplonLatitude, lng: SimplonLongitude},
     disableDefaultUI: true
   }
   var map = new google.maps.Map(document.getElementById("map"),
